7d6ee2414b4a7bf285bb88243415abbb7ff1a2b7
afeiship <1290657123@qq.com>
2021-12-05 19:44:53 +0800

    chore: update ui

--- 05-inline-widget/app.js
+++ 05-inline-widget/app.js
@@ -3,10 +3,11 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Placeholder from './placeholder';
 
 ClassicEditor
   .create(document.querySelector('#editor'), {
-    plugins: [Essentials, Paragraph, Bold, Italic],
+    plugins: [Essentials, Paragraph, Bold, Italic, Placeholder],
     toolbar: ['bold', 'italic']
   })
   .then(editor => {
--- 05-inline-widget/placeholder/index.js
+++ 05-inline-widget/placeholder/index.js
@@ -1,8 +1,8 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-
+import ui from './ui';
 
 export default class Placeholder extends Plugin {
-  static get requires(){
-
+  static get requires () {
+    return [ui];
   }
 }
