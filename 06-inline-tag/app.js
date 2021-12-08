7f2ae8f08dcab6f365d4ad3843a5490ef2f53b47
afeiship <1290657123@qq.com>
2021-12-08 23:44:44 +0800

    feat: inlnie-tag init

---
+++ 06-inline-tag/app.js
@@ -0,0 +1,22 @@
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import InlineTag from './src';
+
+ClassicEditor
+  .create(document.querySelector('#editor'), {
+    plugins: [Essentials, Paragraph, Bold, Italic, InlineTag],
+    toolbar: ['bold', 'italic', 'inline-tag'],
+    placeholderConfig: {
+      types: ['date', 'color', 'first name', 'surname']
+    }
+  })
+  .then(editor => {
+    console.log('Editor was initialized', editor);
+    window.editor = editor;
+  })
+  .catch(error => {
+    console.error(error.stack);
+  });
