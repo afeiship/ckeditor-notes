d23683dfe8ebb9605b28412e02076e9f5db5c791
afeiship <1290657123@qq.com>
2021-12-08 22:00:39 +0800

    feat: add dropdown ivew

--- 05-inline-widget/app.js
+++ 05-inline-widget/app.js
@@ -8,10 +8,14 @@ import Placeholder from './placeholder';
 ClassicEditor
   .create(document.querySelector('#editor'), {
     plugins: [Essentials, Paragraph, Bold, Italic, Placeholder],
-    toolbar: ['bold', 'italic']
+    toolbar: ['bold', 'italic', 'placeholder'],
+    placeholderConfig: {
+      types: ['date', 'color', 'first name', 'surname']
+    }
   })
   .then(editor => {
     console.log('Editor was initialized', editor);
+    window.editor = editor;
   })
   .catch(error => {
     console.error(error.stack);
--- 05-inline-widget/placeholder/ui.js
+++ 05-inline-widget/placeholder/ui.js
@@ -1,7 +1,57 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { addListToDropdown, createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
+
+function getDropdownItemsDefinitions (placeholderNames) {
+  const itemDefinitions = new Collection();
+
+  for (const name of placeholderNames) {
+    const definition = {
+      type: 'button',
+      model: new Model({
+        commandParam: name,
+        label: name,
+        withText: true
+      })
+    };
+
+    // Add the item definition to the collection.
+    itemDefinitions.add(definition);
+  }
+
+  return itemDefinitions;
+}
 
 export default class extends Plugin {
   init () {
     console.log('ui init');
+    const editor = this.editor;
+    const t = editor.t;
+    const names = editor.config.get('placeholderConfig.types');
+    console.log('names:', names);
+
+    editor.ui.componentFactory.add('placeholder', locale => {
+      const dropdownView = createDropdown(locale);
+      console.log(dropdownView);
+      addListToDropdown(dropdownView, getDropdownItemsDefinitions(names));
+      dropdownView.buttonView.set({
+        // The t() function helps localize the editor. All strings enclosed in t() can be
+        // translated and change when the language of the editor changes.
+        label: t('Placeholder'),
+        tooltip: true,
+        withText: true
+      });
+
+
+      // Execute the command when the dropdown item is clicked (executed).
+      this.listenTo(dropdownView, 'execute', evt => {
+        console.log('execute:', evt, evt.source.commandParam);
+        // editor.execute('placeholder', { value: evt.source.commandParam });
+        // editor.editing.view.focus();
+      });
+
+      return dropdownView;
+    });
   }
 }
